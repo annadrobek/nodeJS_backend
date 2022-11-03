6a58d3af69b467ad13fc637faa3d5d47b8ca5ae8
annadrobek <annadrobek@gmail.com>
2022-11-03 21:54:52 +0100

    Dodanie nowego backendu

---
+++ backend_http.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const port = 5000;
+const server = http.createServer(async (req, res) => {
+    if (req.url === "/getServerDate" && req.method === "GET") {
+	let date_ob = new Date();
+        var seconds = date_ob.getSeconds();
+        if (seconds <= 9) {
+           seconds = '0'+seconds;
+        }
+        var minutes = date_ob.getMinutes();
+        if (minutes <= 9) {
+           minutes = '0'+minutes;
+        }
+        var hour = date_ob.getHours();
+        if (hour <= 9) {
+           hour = '0'+hour;
+        }
+        var year = date_ob.getFullYear();
+        var month = date_ob.getMonth()+1;
+        if (month <= 9) {
+           month = '0'+month;
+        }
+        var day = date_ob.getDate();
+        if (day <= 9) {
+           day = '0'+day;
+        }
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(day+'.'+month+'.'+year+' '+hour+':'+minutes+":"+seconds);
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ ServerDate: "Ups coś nie poszło wg planu" }));
+    }
+});
+server.listen(port, () => {
+    console.log('Backend started on port: '+port);
+});
